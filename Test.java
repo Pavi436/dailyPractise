16d7d669989d514816f7ed4ebf7cfe035e2960b5
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-01 09:07:24 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,31 @@
+package com.sample.java;
+
+public class Test {
+	
+	
+public static void main(String[] args) {
+	byte a=3;
+	int b=150;
+	short c=10000;
+	long d=1000000;
+	float e=1.1f;
+	double f=3.9d;
+	boolean g=true;
+	char h= 'a';
+	String j="b";
+	
+System.out.println(j);
+System.out.println(a);	
+System.out.println(b);    
+    
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
