12d843752315135dfff349f55f97eb88f418540b
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-19 18:09:10 +0530

    Add files via upload

--- module-info.java
+++ module-info.java
@@ -1,14 +1,19 @@
-package main.method ;
-public class  {
-	
-public static void main(String[]args) {	
-       String colour="black";
-	   if(colour=="black") {
-		sytem.out.println("if running");   
-	   }
-	   else {
-      system.out.println("else running");
-}	
 
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
