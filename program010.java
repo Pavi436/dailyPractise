eb40a64f16c97d6e267eeec9d2157c07b7e0fad4
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-20 17:09:05 +0530

    Add files via upload

---
+++ program010.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class program010 {
+	    public static void main(String[] args)
+	    {
+	        int a = 2;
+	        int b = 4;
+	        System.out.println("a&b = " + (a & b));
+	        System.out.println("a|b = " + (a | b));
+	        System.out.println("a^b = " + (a ^ b));
+	        System.out.println("~a = " + ~a);
+	        a &= b;
+	        System.out.println("a= " + a);
+	    }
+	}
+	
+	
+	
