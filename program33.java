153be45fee273adc4387a6ecbfb612f07c438693
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-20 18:32:38 +0530

    Add files via upload

---
+++ program33.java
@@ -0,0 +1,13 @@
+public class program33
+	{
+	    public static  void main(String[] args)
+	    {
+	        for(int i = 1; i <= 10; i++)
+	        {
+	            System.out.println(i);
+	        }
+	
+	    }
+	
+	}
+
