4bff602fa0bf03df2ca68aadfedd5da98defa871
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-21 10:08:06 +0530

    Add files via upload

---
+++ program02.java
@@ -0,0 +1,11 @@
+package Basics;
+public class program02 {
+   public static void main(String args[]){
+      
+   int s=13;
+	int area_square=s*s;   
+	System.out.println("Area of the square="+area_square);   
+   }   
+}	   
+	   
+	  	   
