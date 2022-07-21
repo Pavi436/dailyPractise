fe84400ec2a8ba3e7f94863f6761c07104f0ed2b
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-21 15:52:43 +0530

    Add files via upload

---
+++ program220.java
@@ -0,0 +1,25 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program220 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Table Number");
+		int t = s.nextInt();
+
+		for (int i = 0; i <= 5; i++) {
+
+			System.out.println(t + "*" + i + "=" + i * t);
+
+		}
+		int i=0;
+		
+		for ( i = 0; i <= 5; i++) {
+			System.out.println(t + "-" + i + "=" + (t - i));
+		}
+	}
+
+}
