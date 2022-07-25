e8fa79ffa35fe95a6a29fcd9542ad633b9452604
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-25 16:18:07 +0530

    Add files via upload

---
+++ program05.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class program05{
+	 public  static void m1()
+    {
+        System.out.println("from m1");
+    }
+  
+    public static void main(String[] args)
+    {
+           m1();
+    }
+}
+	
