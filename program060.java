3143599868753889f1e865aa171aa6fe63c7c91b
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-26 15:45:29 +0530

    Add files via upload

---
+++ program060.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class program060
+{
+ public static void main(String[]args) {
+int a=4;
+int b=4;
+System.out.println(++a-b--);	
+	
+
+System.out.println(a%b++);
+	
+System.out.println(a*=b+4);	
+	
+System.out.println(a=69>>>2);	
+	
+     }
+
+}
