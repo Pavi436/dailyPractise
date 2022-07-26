d76158a840862b66875384b35c5e1cb95f5cb9cf
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-26 15:58:12 +0530

    Add files via upload

---
+++ program006.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program006
+{
+public static void main (String[]args){
+	Scanner s=new Scanner(System.in);
+	
+	int a=18;
+	System.out.println(a+=a++ + ++a+ --a + a--);
+	
+   }
+	
+	
+}
