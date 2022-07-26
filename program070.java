6cfe2923f420d50ce5d25b76146e3dcf75ad4fde
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-26 16:25:14 +0530

    Add files via upload

---
+++ program070.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program070
+{
+public static void main(String[]args) {	
+	
+	Scanner r=new Scanner(System.in);
+	 
+	char c='z';
+	int  a= c ;
+	
+	System.out.println("The value of "+c+"is"+a);
+	
+   }	
+	
+}
