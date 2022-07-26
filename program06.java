29273566ac5f6715d09de003741e9a36c429a4cd
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-26 10:47:37 +0530

    Add files via upload

---
+++ program06.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class program06{
+	public static void main(String[] args)
+	   {
+	      System.out.println(125 + 463);
+	      System.out.println(2.0 - 6.3);
+	      int age = 65;
+	      System.out.println(age);
+	      System.out.println(age--);
+	      System.out.println(age++);
+	      System.out.println(--age);
+	      System.out.println(++age);
+	      System.out.println("A" + "B");
+	   }
+	}
+	
