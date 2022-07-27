4121eba56e7b705447772b9deda102f1e86c34a5
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 10:38:48 +0530

    Add files via upload

---
+++ program08.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program08
+{
+public static void main(String[]args){	
+	
+	Scanner r= new Scanner(System.in);
+	
+	long num1 = 20l;
+	long num2= -20l;
+	
+	System.out.println("num1="+num1);
+	System.out.println("num2="+num2);
+     
+     }	
+	
+	
+}
