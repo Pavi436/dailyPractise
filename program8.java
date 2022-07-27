5bf577899722733a1f12e2f1c7c241c35b7aa074
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 10:28:03 +0530

    Add files via upload

---
+++ program8.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program8
+{
+public static void main(String[]args) {	
+	
+	Scanner p= new Scanner(System.in);
+	
+	int dividend=70;int divisor=7;
+	int quatient=dividend/divisor;
+	int remainder=dividend%divisor;
+	
+	System.out.println("The divisor is="+quatient);
+	System.out.println("The quatient is="+remainder);
+	}
+		
+}
