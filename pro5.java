ecb49077c915b6074cd521664a86f4f28c117997
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 17:24:01 +0530

    Add files via upload

---
+++ pro5.java
@@ -0,0 +1,34 @@
+	package Basics;
+
+	import java.util.Scanner;
+
+	public class pro5
+	{
+	public static void main(String[]args) {	
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the unit");
+		
+		int units;
+		units=s.nextInt();
+
+		double billpay=0;
+
+	        	if(units<100)
+		 {
+		      billpay=units*1.50;
+		  }
+		else if(units>100 && units<200)
+		 {
+		   billpay=units*3.00;
+		 }
+		else if(units>200)
+		{
+		    billpay=units*5.00;
+		}
+	       
+		System.out.println("Bill to pay : " + billpay); 
+	      } 
+	   }
+	
+
