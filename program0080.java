819b814c0d722f8c088e17bf6020a17cf5d750d1
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 11:27:10 +0530

    Add files via upload

---
+++ program0080.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class program0080
+{
+public static void main(String[]args){	
+	
+Scanner r = new Scanner(System.in);	
+	
+	//char as='i';
+	char al='i';
+	if(al=='a'|| al == 'e' || al == 'i' || al =='0'|| al =='u')
+	
+	System.out.println(al + "is vowel");
+	
+	else {
+	System.out.println(al + " is consonant");
+	
+            }
+	
+      }
+	
+}
