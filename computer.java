175e78683497c0ec73fcc08da4372e10f84eae1b
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 13:28:20 +0530

    Add files via upload

---
+++ computer.java
@@ -0,0 +1,32 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class computer
+{
+public static void main (String[]args){	
+	
+   int a =9876,reversed=0;	
+   
+	for(;a !=0; a /= 5) {
+	
+	int digit= a % 5;
+		
+	reversed = reversed * 5 + digit;	
+		
+	{	
+
+	System.out.println("Reversed number:"+ reversed);
+	
+	      }
+	
+	
+	    }
+	
+	
+	
+	 }
+	
+	
+	
+}
