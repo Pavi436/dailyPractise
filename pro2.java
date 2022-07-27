53eb404f08555a73ef693fd5144082ebcf67c6c0
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 13:29:20 +0530

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class pro2
+{
+public static void main(String[]args) {	
+	int units=75;
+
+	int billpay=0;
+
+        	if(units<100)
+	 {
+	      billpay=units*1.50;
+	  }
+	else if(units<200)
+	 {
+	   billpay=units*1.50;
+	 }
+	else if(units<200)
+	{
+	    billpay=units*1.50;
+	}
+       
+	System.out.println("Bill to pay : " + billpay); 
+      } 
+   }
+	
+
