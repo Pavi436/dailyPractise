928b826e5b7a8670dc28b9882c3bf54bf4c83e22
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-07-27 13:28:59 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,33 @@
+package Basics;
+
+	import java.util.Scanner;
+
+	public class pro1
+	{
+	public static void main (String[]args){	
+		
+	   int a =123456789,reversed=0;	
+	   
+		for(;a !=0; a /= 10) {
+		
+		int digit= a % 10;
+			
+		reversed = reversed * 10+ digit;	
+			
+		{	
+
+		System.out.println("Reversed number:"+ reversed);
+		
+		      }
+		
+		
+		    }
+		
+		
+		
+		 }
+		
+		
+		
+	}	
+	
