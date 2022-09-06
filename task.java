61ea8213251fc749bee35620f63580f87d09a68c
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-09-06 16:58:38 +0530

    task.java

---
+++ task.java
@@ -0,0 +1,13 @@
+package Basics;
+public class task 
+{
+public static void main(String[]args){
+int a = 5;
+int b = 8;
+a = a^b;
+b = b^a;
+a = b^a;
+system.out.println("a =" +a+ "b =" +b);
+     }
+
+}
