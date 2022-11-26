7c2318cfcc1a0cae8f5386c9e52d8a31417e70d2
Pavi436 <108322744+Pavi436@users.noreply.github.com>
2022-11-26 16:44:23 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,39 @@
+public class Square{
+    public static void main(String[]args){
+        /*int n=5;
+        System.out.println("Square root :");
+        while(n!=11){
+            System.out.println(n*n);
+            n++;
+        }/*
+
+
+         */
+
+       /*for(int n=5;n!=11;n++){
+           System.out.println(n*n);
+       }
+/*
+
+        */
+        /*int n=5;
+        do{
+            System.out.println(n*n);
+            n++;
+        }while(n!=11);
+/*
+
+         */
+       int a=20;
+       int b=100;
+
+       if(a<20||b>=100){
+           System.out.println("Square root :");
+           for(int n=5;n!=11;n++){
+               System.out.println(n*n);
+           }
+       }
+
+
+    }
+}
